099f37b6e6c07ce319a8dd2c92451c157f32e17a
byldrma3 <byldrm833@gmail.com>
2023-09-13 20:19:38 +0300

    feat: upload middleware image name changed

--- middleware/uploadMiddleware.ts
+++ middleware/uploadMiddleware.ts
@@ -4,7 +4,8 @@ const storage = multer.diskStorage({
   destination: "./uploads", // Yüklenen fotoğrafların saklanacağı klasör
   filename: function (req, file, cb) {
     const date = new Date().toISOString().split("T")[0];
-    const formattedName = `${date}-${file.originalname}`;
+    const time = new Date().getTime();
+    const formattedName = `${date}-${time}-${file.originalname}`;
     cb(null, formattedName);
   },
 });
